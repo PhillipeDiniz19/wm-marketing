9970f0d5ac2d3b085f64379bca54a9bfd2bbb3a2
PhillipeDiniz19 <phillipediniz19@gmail.com>
2025-03-26 16:24:28 -0300

    abdas

--- index.js
+++ index.js
@@ -21,3 +21,22 @@ var swiper = new Swiper(".mySwiper", {
         }
     }
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll("a[href^='#']").forEach(anchor => {
+        anchor.addEventListener("click", function (event) {
+            event.preventDefault(); // Impede a adição do hash na URL
+
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
